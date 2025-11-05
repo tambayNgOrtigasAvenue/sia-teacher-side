0ee0a84af0348321346fc3db601b848418329604
Mark Kengie Aldabon <markkengiealdabon@gmail.com>
2025-11-05 13:45:10 +0800

    Get current teacher logged in

--- backend/api/auth/get-current-user.php
+++ backend/api/auth/get-current-user.php
@@ -31,57 +31,96 @@ if (!$db) {
 
 $user = new User($db);
 
-$userData = $user->getStudentByUserId($_SESSION['user_id']);
+// Check user type and fetch appropriate data
+$userType = $_SESSION['user_type'] ?? null;
 
-if (!$userData) {
-    http_response_code(404);
-    echo json_encode(['success' => false, 'message' => 'User data not found.']);
-    exit();
+if ($userType === 'Teacher') {
+    // Fetch teacher data
+    $userData = $user->getTeacherByUserId($_SESSION['user_id']);
+    
+    if (!$userData) {
+        http_response_code(404);
+        echo json_encode(['success' => false, 'message' => 'Teacher data not found.']);
+        exit();
+    }
+    
+    http_response_code(200);
+    echo json_encode([
+        'success' => true,
+        'user' => [
+            'userId' => $userData['UserID'],
+            'emailAddress' => $userData['EmailAddress'],
+            'userType' => $userData['UserType'],
+            'accountStatus' => $userData['AccountStatus'],
+            'lastLoginDate' => $userData['LastLoginDate'],
+            'profileId' => $userData['ProfileID'],
+            'firstName' => $userData['FirstName'],
+            'lastName' => $userData['LastName'],
+            'middleName' => $userData['MiddleName'],
+            'fullName' => $userData['FullName'],
+            'phoneNumber' => $userData['PhoneNumber'] ?? null,
+            'address' => $userData['Address'] ?? null,
+            'profilePictureURL' => $userData['ProfilePictureURL'],
+            'teacherProfileId' => $userData['TeacherProfileID'],
+            'employeeNumber' => $userData['EmployeeNumber'],
+            'specialization' => $userData['Specialization'],
+            'hireDate' => $userData['HireDate']
+        ]
+    ]);
+} else {
+    // Fetch student data (original code)
+    $userData = $user->getStudentByUserId($_SESSION['user_id']);
+
+    if (!$userData) {
+        http_response_code(404);
+        echo json_encode(['success' => false, 'message' => 'User data not found.']);
+        exit();
+    }
+
+    http_response_code(200);
+    echo json_encode([
+        'success' => true,
+        'user' => [
+            'userId' => $userData['UserID'],
+            'emailAddress' => $userData['EmailAddress'],
+            'userType' => $userData['UserType'],
+            'accountStatus' => $userData['AccountStatus'],
+            'lastLoginDate' => $userData['LastLoginDate'],
+            'profileId' => $userData['ProfileID'],
+            'firstName' => $userData['FirstName'],
+            'lastName' => $userData['LastName'],
+            'middleName' => $userData['MiddleName'],
+            'fullName' => $userData['FullName'],
+            'phoneNumber' => $userData['PhoneNumber'],
+            'address' => $userData['Address'],
+            'profilePictureURL' => $userData['ProfilePictureURL'],
+            'studentProfileId' => $userData['StudentProfileID'],
+            'studentNumber' => $userData['StudentNumber'],
+            'qrCodeId' => $userData['QRCodeID'],
+            'dateOfBirth' => $userData['DateOfBirth'],
+            'gender' => $userData['Gender'],
+            'nationality' => $userData['Nationality'],
+            'studentStatus' => $userData['StudentStatus'],
+            'age' => $userData['Age'],
+            'schoolYear' => $userData['SchoolYear'],
+            'schoolYearId' => $userData['SchoolYearID'],
+            'gradeLevel' => $userData['GradeLevel'],
+            'sectionName' => $userData['SectionName'],
+            'sectionId' => $userData['SectionID'],
+            'gradeAndSection' => $userData['GradeAndSection'],
+            'adviserName' => $userData['AdviserName'],
+            'weight' => $userData['Weight'],
+            'height' => $userData['Height'],
+            'allergies' => $userData['Allergies'],
+            'medicalConditions' => $userData['MedicalConditions'],
+            'medications' => $userData['Medications'],
+            'emergencyContactPerson' => $userData['EmergencyContactPerson'],
+            'emergencyContactNumber' => $userData['EmergencyContactNumber'],
+            'primaryGuardianName' => $userData['PrimaryGuardianName'],
+            'primaryGuardianRelationship' => $userData['PrimaryGuardianRelationship'],
+            'primaryGuardianContactNumber' => $userData['PrimaryGuardianContactNumber'],
+            'primaryGuardianEmail' => $userData['PrimaryGuardianEmail']
+        ]
+    ]);
 }
-
-http_response_code(200);
-echo json_encode([
-    'success' => true,
-    'user' => [
-        'userId' => $userData['UserID'],
-        'emailAddress' => $userData['EmailAddress'],
-        'userType' => $userData['UserType'],
-        'accountStatus' => $userData['AccountStatus'],
-        'lastLoginDate' => $userData['LastLoginDate'],
-        'profileId' => $userData['ProfileID'],
-        'firstName' => $userData['FirstName'],
-        'lastName' => $userData['LastName'],
-        'middleName' => $userData['MiddleName'],
-        'fullName' => $userData['FullName'],
-        'phoneNumber' => $userData['PhoneNumber'],
-        'address' => $userData['Address'],
-        'profilePictureURL' => $userData['ProfilePictureURL'],
-        'studentProfileId' => $userData['StudentProfileID'],
-        'studentNumber' => $userData['StudentNumber'],
-        'qrCodeId' => $userData['QRCodeID'],
-        'dateOfBirth' => $userData['DateOfBirth'],
-        'gender' => $userData['Gender'],
-        'nationality' => $userData['Nationality'],
-        'studentStatus' => $userData['StudentStatus'],
-        'age' => $userData['Age'],
-        'schoolYear' => $userData['SchoolYear'],
-        'schoolYearId' => $userData['SchoolYearID'],
-        'gradeLevel' => $userData['GradeLevel'],
-        'sectionName' => $userData['SectionName'],
-        'sectionId' => $userData['SectionID'],
-        'gradeAndSection' => $userData['GradeAndSection'],
-        'adviserName' => $userData['AdviserName'],
-        'weight' => $userData['Weight'],
-        'height' => $userData['Height'],
-        'allergies' => $userData['Allergies'],
-        'medicalConditions' => $userData['MedicalConditions'],
-        'medications' => $userData['Medications'],
-        'emergencyContactPerson' => $userData['EmergencyContactPerson'],
-        'emergencyContactNumber' => $userData['EmergencyContactNumber'],
-        'primaryGuardianName' => $userData['PrimaryGuardianName'],
-        'primaryGuardianRelationship' => $userData['PrimaryGuardianRelationship'],
-        'primaryGuardianContactNumber' => $userData['PrimaryGuardianContactNumber'],
-        'primaryGuardianEmail' => $userData['PrimaryGuardianEmail']
-    ]
-]);
 ?>
