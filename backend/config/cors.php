2093f0c7dc72a647ab65748f051f203db0d80642
Mark Kengie Aldabon <markkengiealdabon@gmail.com>
2025-11-13 00:01:50 +0800

    Add allowed origins for tests

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,12 +1,25 @@
 <?php
 
+// List of allowed origins for development testing on phone
+/*
+$allowed_origins = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'http://192.168.100.33:5173',  // Your computer's IP address
+    'http://192.168.56.1:5173',
+    'http://172.24.160.1:5173',
+];
+*/
+
 $allowed_origin = 'http://localhost:5173';
 
-if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] === $allowed_origin) {
+if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] === $allowed_origin){
     header("Access-Control-Allow-Origin: " . $allowed_origin);
-} else {
-    header("Access-Control-Allow-Origin: http://localhost");
 }
+else{
+    header("Access-Control-Allow-Origin: http://localhost:5173");
+}
+
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
